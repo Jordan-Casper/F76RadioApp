768c4adcfb809f5b1ce3d1e6ef06fc378d229833
Jordan-Casper <14353592+Jordan-Casper@users.noreply.github.com>
2024-05-27 22:00:18 -0600

    Added blank templace main.cpp and header

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// main.cpp
+#include <iostream> // for standard input and output
+
+using namespace std; // using the standard namespace
+
+// Function declarations
+void exampleFunction();
+
+// Main function - entry point of the program
+int main() {
+    // Print a message to the console
+    cout << "Hello, World!" << endl;
+
+    // Call an example function
+    exampleFunction();
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
+
+// Function definitions
+void exampleFunction() {
+    cout << "This is an example function." << endl;
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+// myheader.h
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+// Function declarations
+void printMessage();
+int add(int a, int b);
+
+#endif // MYHEADER_H
